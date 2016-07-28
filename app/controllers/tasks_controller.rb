1aca413bb2b09eacc62a266492f83914e42eb44f
Aaron Groch <ar.groch@gmail.com>
2016-07-28 08:35:12 -0400

    homework for w5/d3 - putting tasks in order by date/time due

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,16 +3,31 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:edit, :update, :destroy]
 
 
+  def same_day_tasks
+    @tasks = []
+    @date = params[:deadline][0..9]
+
+    Task.all.each do |task|
+      if task.deadline[0..9] == @date
+        @tasks.push(task)
+      end
+    end
+  end
+
   def new
     @task = Task.new
   end
 
   def create
     @task = Task.create(task_params)
+    @task.dl_dt = @task.datified_dl
+    @task.save
   end
 
   def update
     @task.update(task_params)
+    @task.dl_dt = @task.datified_dl
+    @task.save
   end
 
   def destroy
@@ -23,13 +38,13 @@ class TasksController < ApplicationController
     # The Lounge - welcome!
     # Use callbacks to share common setup or constraints between actions.
     def all_tasks
-      @tasks = Task.order(:deadline)
+      @tasks = Task.order(:dl_dt)
+      # @tasks = Task.all.sort_by { |task| task.datified_dl }
     end 
 
     def set_task
       @task = Task.find(params[:id])
     end
-
     
 
     # Never trust parameters from the scary internet, only allow the white list through.
